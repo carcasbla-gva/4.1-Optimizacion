cfdfe2e62981f2d57db29a30879f40534728d304
carlitic <carloscastanosblanco@gmail.com>
2026-02-16 00:05:03 +0100

    EjercicioC acabado

--- src/main/java/EjercicioC.java
+++ src/main/java/EjercicioC.java
@@ -17,5 +17,16 @@ public class EjercicioC {
         System.out.println("Recalcular dentro: " + (t2 - t1) + " ns");
 
         // TODO: Después: mueve el cálculo fuera del bucle y vuelve a medir
+        long t3 = System.nanoTime();
+        int totalOptimizado = 0;
+        int precioConIVAFuera = (int) (precio * 1.21);
+
+        for (int i = 0; i < unidades; i++) {
+            totalOptimizado += precioConIVAFuera;
+        }
+        long t4 = System.nanoTime();
+
+        System.out.println("Total: " + totalOptimizado);
+        System.out.println("Recalcular fuera: " + (t4 - t3) + " ns");
     }
 }
